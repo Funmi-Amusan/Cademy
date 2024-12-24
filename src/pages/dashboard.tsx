e98136c119662cba08a4d2edd91ce5a7f157a1a4
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-12-24 23:01:43 +0100

    dshboard sections

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,7 +6,20 @@ const Dashboard = () => {
     return (
       <Layout>
         <Header />
-        <h1>Dashboard</h1>
+        <main className=" grid grid-cols-3 ">
+            <div className=" col-span-2 bg-purple-500" >
+        {/* Conditional reminder if task is in less than 3 hours */}
+        <h1>reminder</h1>
+        {/* Enrolled courses */}
+        <h1>Courses</h1>
+            </div>
+            <div className=" col-span-1 bg-blue-400">
+                 {/* Fancy calendar component */}
+        <h1>calendar</h1>
+        {/* Tasks in timeline view */}
+        <h1>tasks</h1>
+            </div>
+        </main>
       </Layout>
     );
 };
