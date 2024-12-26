025b580f959374e12338268e520851dbdb423279
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-12-26 04:53:38 +0100

    events label design change

--- src/components/calendar/UpcomingClasses.tsx
+++ src/components/calendar/UpcomingClasses.tsx
@@ -20,7 +20,7 @@ function UpcomingClasses() {
     <div className="flex items-center justify-between">
 
     <h2>Fundamentals of Physics</h2>
-    <p className=" px-2 py-1 bg-teal-400 rounded-sm">Lecture</p>
+    <p className=" px-4 py-1 bg-teal-100 text-teal-700 font-bold rounded-md">Lecture</p>
     </div>
     <div className=" flex items-center gap-2">
     <GoClock />
