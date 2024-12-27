0cf39e5ff82cd5d65cfc6db5ab34bd2502a669b5
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-12-27 22:52:18 +0100

    header component

---
+++ src/components/schedule/Header.tsx
@@ -0,0 +1,57 @@
+import React, { useContext, useState } from "react";
+import { format, getYear, setMonth } from "date-fns";
+import logo from "../assets/logo.png";
+// import GlobalContext from "../../context/";
+
+export default function CalendarHeader() {
+  const { monthIndex, setMonthIndex } = useState(1);
+
+//   function handlePrevMonth() {
+//     setMonthIndex(monthIndex - 1);
+//   }
+
+//   function handleNextMonth() {
+//     setMonthIndex(monthIndex + 1);
+//   }
+
+//   function handleReset() {
+//     const currentMonth = new Date().getMonth();
+//     setMonthIndex(
+//       monthIndex === currentMonth
+//         ? monthIndex + Math.random()
+//         : currentMonth
+//     );
+//   }
+
+  return (
+    <header className="px-4 py-2 flex items-center">
+      <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
+      <h1 className="mr-10 text-xl text-gray-500 fond-bold">
+        Calendar
+      </h1>
+      <button
+        //onClick={handleReset}
+        className="border rounded py-2 px-4 mr-5"
+      >
+        Today
+      </button>
+      <button 
+      //onClick={handlePrevMonth}
+      >
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+          chevron_left
+        </span>
+      </button>
+      <button 
+      //onClick={handleNextMonth}
+      >
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+          chevron_right
+        </span>
+      </button>
+      <h2 className="ml-4 text-xl text-gray-500 font-bold">
+        {format(setMonth(new Date(getYear(new Date()), monthIndex), monthIndex), 'MMMM yyyy')}
+      </h2>
+    </header>
+  );
+}
