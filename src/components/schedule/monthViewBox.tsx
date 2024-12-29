28a8880648d9339b0c1e1887079ef773cae1e602
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-12-29 11:30:58 +0100

    style current day

--- src/components/schedule/monthViewBox.tsx
+++ src/components/schedule/monthViewBox.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { isSameDay } from "date-fns";
+
 
 function MonthViewBox({day, rowIndex, index}: { day: Date, rowIndex: number, index: number}) {
+    const isCurrentDay = isSameDay( day, new Date())
+    
   return (
     <div className='w-8 aspect-square border flex flex-col items-center hover:bg-zinc-200 transition-all'>
 {rowIndex === 0 && (
@@ -9,9 +12,8 @@ function MonthViewBox({day, rowIndex, index}: { day: Date, rowIndex: number, ind
      } )}</h3>
 )
 
-
 }
-<h3>{day.toLocaleDateString('en-NG', {
+<h3 className={` ${isCurrentDay ? 'bg-teal-600 text-white rounded-full p-2 ' : ''}`}>{day.toLocaleDateString('en-NG', {
     day: 'numeric'
  } )}</h3>
     </div>
